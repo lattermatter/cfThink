8de6504a9ba7e1278c01829c6dff82d7e58eec41
lattermatter <sanjaysreddy2357@gmail.com>
2026-06-30 13:45:58 -0700

    working frontend-backend connection, basic

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"], 
+)
+
+
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def combine_string(userString: str = "", userNum: int = 1):
+    return {"output": "Contacted Backend! " + userString*userNum}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,14 +8,6 @@ const processCountInput = (value: number): number => {
   return value
 }
 
-const outputString = (str: string, val: number): string => {
-  let outv: string = '';
-  for (let i = 0; i < val; i++)
-    outv += (str);
-
-  return outv;
-}
-
 function App() {
   const [userString, setUserString] = useState("")
   const [userCount, setUserCount] = useState(1)
@@ -46,9 +38,24 @@ function App() {
           <button
             type="button"
             className="submit"
-            onClick={() => {
-              setDisplayString(outputString(userString, userCount))
-              console.log("here")
+            onClick={async () => {
+              const params = new URLSearchParams({
+                userString: userString,
+                userNum: userCount.toString()
+              })
+              try {
+                const queryStringConcat = `http://localhost:8080/?${params.toString()}`;
+                const response = await fetch(queryStringConcat);
+                if (!response.ok) {
+                  throw new Error(`Response status: ${response.status}`);
+                }
+
+                const result = await response.json();
+                setDisplayString(result.output)
+              } catch (error) {
+                if (error instanceof Error)
+                  console.error(error.message);
+              }
             }}
           >
             Submit
